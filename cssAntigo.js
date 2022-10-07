0d4d278e2209e24beb0d1eec785534c35a156933
Joao <joaoeduardorrocha@gmail.com>
2022-10-07 20:49:04 -0300

    first commity

---
+++ cssAntigo.js
@@ -0,0 +1,111 @@
+/* * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.page {
+    max-width: 750px;
+    width: 100%;
+    margin: 0 auto;
+}
+form {
+    background: white;
+
+    margin-top: 32px;
+
+    border-radius: 21px;
+}
+
+header {
+    width: 1440px;
+    margin-top: 107px;
+}
+
+fieldset {
+    border: none;
+    margin: 32px 64px 64px 64px;
+}
+
+.input-wrapper {
+    display: flex;
+}
+
+h1 {
+    margin: 107px 0 auto;
+    font-family: 'Staatliches', 'san-serif';
+    font-size: 36px;
+    background: #293241;
+}
+
+p, label, input {
+    font-family: 'Poppins', 'san-serif';
+    font-size: 14px;
+}
+
+p {
+    background: #293241;
+    margin-bottom: 2px;
+}
+
+legend {
+    font-family: 'Staatliches', 'san-serif';
+    font-size: 24px;
+    line-height: 24px;
+}
+
+label {
+    font-family: 'Poppins', 'san-serif';
+    font-size: 14px;
+    line-height: 24px;
+}
+
+body {
+    background-color: #BFD7E4;
+}
+
+body::before {
+    content: "";
+    width: 100%;
+    height: 465px;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    background-color: #293241;
+}
+
+.legend1 {
+    text-align: center;
+
+    background:#F67669;
+
+    border-radius: 20px 20px 0 0;
+}
+
+
+.legend1, h1, p {
+   color: white;
+   line-height: 34px;
+}
+
+.legend2 {
+    color: #253B51;
+    line-height: 34px;
+
+    border-bottom: 1px solid #E6E6F0;
+    width: 100%;
+
+    padding-bottom: 8px;
+}
+
+.col-3 {
+    display: flex;
+
+}
+
+.icon {
+    margin-bottom: 32px;
+}
+
+*/
